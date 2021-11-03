a5a1dd5704616e6e9f201e03288269b27bb4c362
JeyNessuno <jey.jey66611@gmail.com>
2021-11-03 21:10:40 +0100

    Add files via upload

---
+++ gioco.bat
@@ -0,0 +1,16 @@
+@echo off
+color b
+echo JeyNessuno's giochi tristi presents...
+pause;
+:Menu
+cls
+echo Dite, amici, ed entrate!
+set /p mellon=
+if %mellon%==mellon goto 1
+pause;
+goto Menu
+
+:1
+echo Benvenuto nelle miniere di Moria!
+pause;
+cls
